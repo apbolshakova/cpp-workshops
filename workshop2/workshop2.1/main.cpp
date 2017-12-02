1ef1b845ec7d5ba6cbb7a6148afd2d2d7b3d2709
julody <julody@mail.ru>
2017-12-02 09:43:27 +0300

    workshop2.1: шары рендерятся, но не меняют цвет и положение

--- workshop2/workshop2.1/main.cpp
+++ workshop2/workshop2.1/main.cpp
@@ -1,13 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <iostream>
 
 struct Ball
 {
-    sf::CircleShape circleShape[30];
+    sf::CircleShape circleShape;
     sf::Vector2f speed;
     sf::Color color;
-}
+};
 
 main()
 {
@@ -15,7 +16,8 @@ main()
     const int WINDOW_HEIGHT = 600;
 
     Ball balls[] = {
-        {, {50.f, 15.f}, sf::Color::Red}};
+        {sf::CircleShape(30), {50.f, 15.f}, sf::Color::Red},
+        {sf::CircleShape(50), {40.f, 30.f}, sf::Color::Blue}};
 
     sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Moving Balls");
     sf::Clock clock;
@@ -34,10 +36,11 @@ main()
         const float deltaTime = clock.restart().asSeconds();
         for (Ball ball : balls)
         {
+            int ballSize = ball.circleShape.getRadius();
             sf::Vector2f position = ball.circleShape.getPosition();
             position += ball.speed * deltaTime;
 
-            if ((position.x + 2 * BALL_SIZE >= WINDOW_WIDTH) && (ball.speed.x > 0))
+            if ((position.x + 2 * ballSize >= WINDOW_WIDTH) && (ball.speed.x > 0))
             {
                 ball.speed.x = -ball.speed.x;
             }
@@ -45,7 +48,7 @@ main()
             {
                 ball.speed.x = -ball.speed.x;
             }
-            if ((position.y + 2 * BALL_SIZE >= WINDOW_HEIGHT) && (ball.speed.y > 0))
+            if ((position.y + 2 * ballSize >= WINDOW_HEIGHT) && (ball.speed.y > 0))
             {
                 ball.speed.y = -ball.speed.y;
             }
